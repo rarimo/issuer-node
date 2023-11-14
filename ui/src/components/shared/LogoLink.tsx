373c062c9cc637a2066e7b5cd03ec762ba97e7fe
roman_harazha <roman.harazha@gmail.com>
2023-11-14 18:00:15 +0200

    fix UI logo

--- ui/src/components/shared/LogoLink.tsx
+++ ui/src/components/shared/LogoLink.tsx
@@ -4,9 +4,9 @@ import { ReactComponent as IconLogo } from "src/assets/rarimo-logo.svg";
 import { ROOT_PATH } from "src/utils/constants";
 
 export function LogoLink() {
-  return (
-    <Link to={ROOT_PATH}>
-      <IconLogo />
-    </Link>
-  );
+    return (
+        <Link to={ROOT_PATH}>
+            <IconLogo className='app-logo' />
+        </Link>
+    );
 }
